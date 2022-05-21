08d654e0639a6186e206ef477826f4429ca2c436
7 <7@sevvie.ltd>
2022-05-20 17:29:51 -0700

    chore: init via `minil new`

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+WWW::Fedi - It's new $module
+
+# SYNOPSIS
+
+    use WWW::Fedi;
+
+# DESCRIPTION
+
+WWW::Fedi is ...
+
+# LICENSE
+
+Copyright (C) 7.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+7 <7@sevvie.ltd>
---
+++ lib/WWW/Fedi.pm
@@ -0,0 +1,39 @@
+package WWW::Fedi;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::Fedi - It's new $module
+
+=head1 SYNOPSIS
+
+    use WWW::Fedi;
+
+=head1 DESCRIPTION
+
+WWW::Fedi is ...
+
+=head1 LICENSE
+
+Copyright (C) 7.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+7 E<lt>7@sevvie.ltdE<gt>
+
+=cut
+
